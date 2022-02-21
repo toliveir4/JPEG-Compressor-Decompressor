24511cc73892155d4fe4ace9e7c0760ef79a56b6
Tiago Oliveira <tiago.o.tocha@gmail.com>
2022-02-21 20:15:01 +0000

    Co-authored-by: MarcoPais0 <MarcoPais0@users.noreply.github.com>

--- Trabalho1.py
+++ Trabalho1.py
@@ -4,19 +4,26 @@ import numpy as np
 
 
 def encoder():
-    img = plt.imread('imagens/logo.bmp')
+    img = plt.imread('imagens/peppers.bmp')
 
-    #cm = getColormap()
+    # plt.figure()
+    # plt.imshow(img)
 
-    #viewColormap(cm, img)
+    # cm = getColormap()
+
+    # viewColormap(cm, img)
     
     R, G, B = separateRGB(img)
-    
-    RGB = joinRGB(R, G, B)
 
-    #viewChanels(R, G, B)
+    # viewChanels(R, G, B)
 
     addPadding(img)
+
+    YCbCr = RGBtoYCbCr(R, G, B)
+
+    # showYCbCr(YCbCr)
+
+    return R, G, B, YCbCr
 
 
 def getColormap():
@@ -32,10 +39,6 @@ def viewColormap(cm, img):
 
 
 def viewChanels(R, G, B):
-    cmr = clr.LinearSegmentedColormap.from_list('cmr', [(0, 0, 0), (1, 0, 0)], N=256)
-    cmg = clr.LinearSegmentedColormap.from_list('cmg', [(0, 0, 0), (0, 1, 0)], N=256)
-    cmb = clr.LinearSegmentedColormap.from_list('cmb', [(0, 0, 0), (0, 0, 1)], N=256)
-
     plt.figure()
     plt.imshow(R, cmr)
     
@@ -54,18 +57,8 @@ def separateRGB(img):
     return R, G, B
 
 
-def joinRGB(R, G, B):
-    RGB = np.dstack((R, G, B))
-
-    return RGB
-
-
 def addPadding(img):
     h, w, d = np.shape(img)
-
-    print("%d, %d" % (h, w))
-    plt.figure()
-    plt.imshow(img)
 
     if h % 16 != 0:
         add = 16 - (h % 16)
@@ -79,23 +72,78 @@ def addPadding(img):
         arr[w-1] = add + 1
         img = np.repeat(img, arr, axis=1)
 
-    print(np.shape(img))
+
+def RGBtoYCbCr(R, G, B):
+    matrix = np.array([[0.299, 0.587, 0.114], 
+                    [-0.168736, -0.331264, 0.5], 
+                    [0.5, -0.418688, -0.081312]])
+
+    RGB = joinRGB(R, G, B)
+
+    YCbCr = RGB.dot(matrix)
+    YCbCr[:, :, [1, 2]] += 128
+
+    return YCbCr
+
+
+def decoder(R, G, B, YCbCr):
+    RGBAfter = YCbCrtoRGB(YCbCr)
+    RGBBefore = joinRGB(R, G, B)
+    
+    # plt.figure()
+    # plt.imshow(RGBAfter)
+
+    # comp = RGBAfter == RGBBefore
+    # res = comp.all()
+    # print(res)
+
+def joinRGB(R, G, B):
+    RGB = np.dstack((R, G, B))
+
+    return RGB
+
+
+def YCbCrtoRGB(YCbCr):
+    matrix = np.array([[0.299, 0.587, 0.114], 
+                    [-0.168736, -0.331264, 0.5], 
+                    [0.5, -0.418688, -0.081312]])
+
+    inverted = np.linalg.inv(matrix)
+    YCbCr[:, :, [1, 2]] -= 128
+    RGB = YCbCr.dot(inverted)
+    RGB = RGB.round()
+    RGB[RGB > 255] = 255
+    RGB[RGB < 0] = 0
+    RGB = RGB.astype(np.uint8)
+
+    return RGB
+
+
+def showYCbCr(YCbCr):
+    Y = YCbCr[:, :, 0]
+    Cb = YCbCr[:, :, 1]
+    Cr = YCbCr[:, :, 2]
+
     plt.figure()
-    plt.imshow(img)
-
-
-
-def decoder():
-    return
+    plt.imshow(Y, cmGray)
+    plt.figure()
+    plt.imshow(Cb, cmGray)
+    plt.figure()
+    plt.imshow(Cr, cmGray)
 
 
 def main():
     plt.close('all')
 
-    encoder()
-    
+    R, G, B, YCbCr = encoder()
+    decoder(R, G, B, YCbCr)
+
     plt.show()
 
 
 if __name__ == '__main__':
+    cmr = clr.LinearSegmentedColormap.from_list('cmr', [(0, 0, 0), (1, 0, 0)], N=256)
+    cmg = clr.LinearSegmentedColormap.from_list('cmg', [(0, 0, 0), (0, 1, 0)], N=256)
+    cmb = clr.LinearSegmentedColormap.from_list('cmb', [(0, 0, 0), (0, 0, 1)], N=256)
+    cmGray = clr.LinearSegmentedColormap.from_list('cmGray', [(0, 0, 0), (1, 1, 1)], N=256)
     main()
