fe3d64ef000ef97c1a67eccf32f4c525d6cd3d29
Tiago Oliveira <tiago.o.tocha@gmail.com>
2022-03-14 02:14:22 +0000

    Quantization and some changes in DCT

--- Trabalho1.py
+++ Trabalho1.py
@@ -1,13 +1,17 @@
-import matplotlib.colors as clr
+import matplotlib.colors as cb_linesr
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.fftpack import dct, idct
 
 
-def encoder(): # 2
+def encoder():  # 2
     img_name = str(input("Image name: "))
+
+    global dSample, opt
     dSample = int(input("Downsample: "))
-    img = plt.imread(f'imagens/{img_name}.bmp') # 3.1
+    opt = int(input("DCT block size: "))
+
+    img = plt.imread(f'imagens/{img_name}.bmp')  # 3.1
 
     '''plt.figure()
     plt.imshow(img)
@@ -17,40 +21,42 @@ def encoder(): # 2
 
     # viewColormap(cm, img) # 3.3
 
-    img = addPadding(img) # 4.1
+    img = addPadding(img)  # 4.1
 
-    R, G, B = separateRGB(img) # 3.4
+    R, G, B = separateRGB(img)  # 3.4
 
     # viewChanels(R, G, B) # 3.5
-    
-    YCbCr = RGBtoYCbCr(R, G, B) # 5
+
+    YCbCr = RGBtoYCbCr(R, G, B)  # 5
 
     # showYCbCr(YCbCr) # 5
 
-    YD, CbD, CrD = downSample(YCbCr, dSample) # 6
+    YD, CbD, CrD = downSample(YCbCr, dSample)  # 6
 
     # showDownSample(YD, CbD, CrD) # 6
 
-    # Y_dct, Cb_dct, Cr_dct = calcDCT(YD, CbD, CrD) # 7.1
-    Y_dct, Cb_dct, Cr_dct = calcDCT_8x8_64x64(YD, CbD, CrD, option=8) # 7.2 and 7.3
-    # showDCT(Y_dct, Cb_dct, Cr_dct) # 7.1.2
+    Y_dct, Cb_dct, Cr_dct = calcDCT_8x8_64x64(YD, CbD, CrD)  # 7
+    showDCT(Y_dct, Cb_dct, Cr_dct) # 7.1.2
 
-    return Y_dct, Cb_dct, Cr_dct, dSample
+    Y_Q, Cb_Q, Cr_Q = quantization(Y_dct, Cb_dct, Cr_dct) # 8
+
+    return Y_Q, Cb_Q, Cr_Q
 
 
-def getColormap(): # 3.2
+def getColormap():  # 3.2
     inp = str(input("Introduza colormap (r, g, b): "))
     r, g, b = inp.split(",")
-    cm = clr.LinearSegmentedColormap.from_list('cm', [(0, 0, 0), (int(r), int(g), int(b))], N=256)
+    cm = cb_linesr.LinearSegmentedColormap.from_list(
+        'cm', [(0, 0, 0), (int(r), int(g), int(b))], N=256)
     return cm
 
 
-def viewColormap(cm, img): # 3.3
+def viewColormap(cm, img):  # 3.3
     plt.figure()
     plt.imshow(img, cm)
 
 
-def viewChanels(R, G, B): # 3.5
+def viewChanels(R, G, B):  # 3.5
     fig = plt.figure(figsize=(10, 5))
     fig.add_subplot(131)
     plt.title("Red")
@@ -63,7 +69,7 @@ def viewChanels(R, G, B): # 3.5
     plt.imshow(B, cmb)
 
 
-def separateRGB(img): # 3.4
+def separateRGB(img):  # 3.4
     R = img[:, :, 0]
     G = img[:, :, 1]
     B = img[:, :, 2]
@@ -71,28 +77,32 @@ def separateRGB(img): # 3.4
     return R, G, B
 
 
-def addPadding(img): # 4.1
+def addPadding(img):  # 4.1
     global h, w
     h, w, _ = np.shape(img)
+    if opt == 64:
+        pad = 128
+    else:
+        pad = 16
 
-    if h % 16 != 0:
-        add = 16 - (h % 16)
+    if h % pad != 0:
+        add = pad - (h % pad)
         arr = np.ones(h, dtype=int)
         arr[h-1] = add + 1
         img = np.repeat(img, arr, axis=0)
 
-    if w % 16 != 0:
-        add = 16 - (w % 16)
+    if w % pad != 0:
+        add = pad - (w % pad)
         arr = np.ones(w, dtype=int)
         arr[w-1] = add + 1
         img = np.repeat(img, arr, axis=1)
     return img
 
 
-def RGBtoYCbCr(R, G, B): # 5
-    matrix = np.array([[0.299, 0.587, 0.114], 
-                    [-0.168736, -0.331264, 0.5], 
-                    [0.5, -0.418688, -0.081312]])
+def RGBtoYCbCr(R, G, B):  # 5
+    matrix = np.array([[0.299, 0.587, 0.114],
+                       [-0.168736, -0.331264, 0.5],
+                       [0.5, -0.418688, -0.081312]])
 
     RGB = joinRGB(R, G, B)
 
@@ -106,11 +116,11 @@ def separateYCbCr(YCbCr):
     Y = YCbCr[:, :, 0]
     Cb = YCbCr[:, :, 1]
     Cr = YCbCr[:, :, 2]
-    
+
     return Y, Cb, Cr
 
 
-def downSample(YCbCr, dSample): # 6
+def downSample(YCbCr, dSample):  # 6
     Y, Cb, Cr = separateYCbCr(YCbCr)
 
     Cb = Cb[:, ::2]
@@ -123,7 +133,7 @@ def downSample(YCbCr, dSample): # 6
     return Y, Cb, Cr
 
 
-def showDownSample(YD, CbD, CrD): # 6
+def showDownSample(YD, CbD, CrD):  # 6
     fig = plt.figure(figsize=(10, 5))
     plt.title("YCbCr")
     plt.axis("off")
@@ -138,47 +148,32 @@ def showDownSample(YD, CbD, CrD): # 6
     plt.imshow(CrD, cmGray)
 
 
-def calcDCT(YD, CbD, CrD): # 7.1
-    Y_dct = dct(dct(YD, norm='ortho').T, norm='ortho').T
-    Cb_dct = dct(dct(CbD, norm='ortho').T, norm='ortho').T
-    Cr_dct = dct(dct(CrD, norm='ortho').T, norm='ortho').T
-
-    return Y_dct, Cb_dct, Cr_dct
-
-
-def calcDCT_8x8_64x64(YD, CbD, CrD, option): # 7.2 and 7.3
-    if option == 8 or option == 64:
-        Y_lines, Y_cols = np.shape(YD) 
+def calcDCT_8x8_64x64(YD, CbD, CrD):  # 7
+    if opt == 8 or opt == 64:
+        Y_lines, Y_cols = np.shape(YD)
         Cb_lines, Cb_cols = np.shape(CbD)
 
-        Yx = Y_lines // option
-        Yy = Y_cols // option
-        Cx = Cb_lines // option
-        Cy = Cb_cols // option
+        Y_dct = YD
+        Cb_dct = CbD
+        Cr_dct = CrD
 
-        Y_sliced = YD.reshape(Yx * Yy, option, option)
-        Cb_sliced = CbD.reshape(Cx * Cy, option, option)
-        Cr_sliced = CrD.reshape(Cx * Cy, option, option)
+        for i in range(0, Y_lines, opt):
+            for j in range(0, Y_cols, opt):
+                Y_dct[i:i+opt, j:j+opt] = dct(dct(Y_dct[i:i + opt,j:j + opt], norm="ortho").T, norm="ortho").T
 
-        i = 0
-        for i in range(len(Y_sliced)):
-            Y_sliced[i] = dct(dct(Y_sliced[i], norm="ortho").T, norm="ortho").T
-            i += 1
-
-        i = 0
-        for i in range(len(Cb_sliced)):
-            Cb_sliced[i] = dct(dct(Cb_sliced[i], norm="ortho").T, norm="ortho").T
-            Cr_sliced[i] = dct(dct(Cr_sliced[i], norm="ortho").T, norm="ortho").T
-            i += 1
-        
-        Y_dct = Y_sliced.reshape(Y_lines, Y_cols)
-        Cb_dct = Cb_sliced.reshape(Cb_lines, Cb_cols)
-        Cr_dct = Cr_sliced.reshape(Cb_lines, Cb_cols)
+        for i in range(0, Cb_lines, opt):
+            for j in range(0, Cb_cols, opt):
+                Cb_dct[i:i + opt, j:j + opt] = dct(dct(Cb_dct[i:i + opt, j:j + opt], norm="ortho").T, norm="ortho").T
+                Cr_dct[i:i + opt, j:j + opt] = dct(dct(Cr_dct[i:i + opt, j:j + opt], norm="ortho").T, norm="ortho").T
+    else:
+        Y_dct = dct(dct(YD, norm='ortho').T, norm='ortho').T
+        Cb_dct = dct(dct(CbD, norm='ortho').T, norm='ortho').T
+        Cr_dct = dct(dct(CrD, norm='ortho').T, norm='ortho').T
     
     return Y_dct, Cb_dct, Cr_dct
 
 
-def showDCT(Y_dct, Cb_dct, Cr_dct): # 7.1.2
+def showDCT(Y_dct, Cb_dct, Cr_dct):  # 7.1.2
     fig = plt.figure(figsize=(12, 5))
     fig.add_subplot(131)
     plt.title("Y_DCT")
@@ -190,65 +185,99 @@ def showDCT(Y_dct, Cb_dct, Cr_dct): # 7.1.2
     plt.title("Cr_DCT")
     plt.imshow(np.log(abs(Cr_dct) + 0.0001), cmGray)
 
-    
+
 def getQuantizationMatrix(requiredQuality=50):      # 8
-    qm = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
-                  [12, 12, 14, 19, 26, 58, 60, 55],
-                  [14, 13, 16, 24, 40, 57, 69, 56],
-                  [14, 17, 22, 29, 51, 87, 80, 62],
-                  [18, 22, 37, 56, 68, 109, 103, 77],
-                  [24, 35, 55, 64, 81, 104, 113, 92],
-                  [49, 64, 78, 87, 103, 121, 120, 101],
-                  [72, 92, 95, 98, 112, 100, 103, 99]])
+    Q_Y = np.array([[16, 11, 10, 16, 24, 40, 51, 61],
+                    [12, 12, 14, 19, 26, 58, 60, 55],
+                    [14, 13, 16, 24, 40, 57, 69, 56],
+                    [14, 17, 22, 29, 51, 87, 80, 62],
+                    [18, 22, 37, 56, 68, 109, 103, 77],
+                    [24, 35, 55, 64, 81, 104, 113, 92],
+                    [49, 64, 78, 87, 103, 121, 120, 101],
+                    [72, 92, 95, 98, 112, 100, 103, 99]])
+
+    Q_CbCr = np.array([[17, 18, 24, 47, 99, 99, 99, 99],
+                       [18, 21, 26, 66, 99, 99, 99, 99],
+                       [24, 26, 56, 99, 99, 99, 99, 99],
+                       [47, 66, 99, 99, 99, 99, 99, 99],
+                       [99, 99, 99, 99, 99, 99, 99, 99],
+                       [99, 99, 99, 99, 99, 99, 99, 99],
+                       [99, 99, 99, 99, 99, 99, 99, 99],
+                       [99, 99, 99, 99, 99, 99, 99, 99]])
 
     if requiredQuality == 50:
-        return qm
+        return Q_Y, Q_CbCr
     elif requiredQuality > 50:
-        qm = (qm * ((100 - requiredQuality) / 50)).astype('int')
-        qm = np.where(qm > 255, 255, qm)
-        return qm
-    else:
-        qm = (qm * (50 / requiredQuality)).astype('int')
-        qm = np.where(qm > 255, 255, qm)
-        return qm
-    
+        Q_Y = (Q_Y * ((100 - requiredQuality) / 50)).astype(np.uint8)
+        Q_Y[Q_Y > 255] = 255
 
-def decoder(Y_dct, Cb_dct, Cr_dct, dSample): # 2
-    # Y_enc, Cb_enc, Cr_enc = calcIDCT(Y_dct, Cb_dct, Cr_dct) # 7.1
-    Y_enc, Cb_enc, Cr_enc = calcIDCT_8x8_64x64(Y_dct, Cb_dct, Cr_dct, option=8) # 7.2 and 7.3
-    
-    YCbCrU = upSample(Y_enc, Cb_enc, Cr_enc, dSample) # 6
+        Q_CbCr = (Q_CbCr * ((100 - requiredQuality) / 50)).astype(np.uint8)
+        Q_CbCr[Q_CbCr > 255] = 255
+        return Q_Y, Q_CbCr
+    else:
+        Q_Y = (Q_Y * (50 / requiredQuality)).astype(np.uint8)
+        Q_Y[Q_Y > 255] = 255
+
+        Q_CbCr = (Q_CbCr * (50 / requiredQuality)).astype(np.uint8)
+        Q_CbCr[Q_CbCr > 255] = 255
+        return Q_Y, Q_CbCr
+
+
+def quantization(Y_dct, Cb_dct, Cr_dct):    # 8
+    Q_Y, Q_CbCr = getQuantizationMatrix(requiredQuality=75)
+
+    Y_lines, Y_cols = np.shape(Y_dct)
+    Cb_lines, Cb_cols = np.shape(Cb_dct)
+
+    for i in range(0, Y_lines, opt):
+        for j in range(0, Y_cols, opt):
+            Y_dct[i:i + opt, j:j + opt] = np.round(Y_dct[i:i + opt, j:j + opt] / Q_Y)
+                
+    for i in range(0, Cb_lines, opt):
+        for j in range(0, Cb_cols, opt):
+            Cb_dct[i:i + opt, j:j + opt] = np.round(Cb_dct[i:i + opt, j:j + opt] / Q_CbCr)
+            Cr_dct[i:i + opt, j:j + opt] = np.round(Cr_dct[i:i + opt, j:j + opt] / Q_CbCr)
+
+    return Y_dct, Cb_dct, Cr_dct
+
+
+def decoder(Y_Q, Cb_Q, Cr_Q):  # 2
+    Y_dct, Cb_dct, Cr_dct = deQuantization(Y_Q, Cb_Q, Cr_Q) # 8
+
+    Y_enc, Cb_enc, Cr_enc = calcIDCT_8x8_64x64(Y_dct, Cb_dct, Cr_dct)  # 7
+
+    YCbCrU = upSample(Y_enc, Cb_enc, Cr_enc, dSample)  # 6
 
     # showYCbCr(YCbCrU) # 6
 
-    RGBAfter = YCbCrtoRGB(YCbCrU) # 5
-    
+    RGBAfter = YCbCrtoRGB(YCbCrU)  # 5
+
     # RGBBefore = joinRGB(R, G, B) # 3.4
-    
+
     '''plt.figure()
     plt.imshow(RGBAfter)'''
 
     '''comp = RGBAfter == RGBBefore
     res = comp.all()
     print(res)'''
-    
+
     RGBAfter = unpadding(RGBAfter)  # 4
-    
+
     plt.figure()
     plt.title("Imagem reconstruida")
     plt.imshow(RGBAfter)
     plt.axis('off')
 
 
-def joinRGB(R, G, B): # 3.4
+def joinRGB(R, G, B):  # 3.4
     RGB = np.dstack((R, G, B))
 
     return RGB
 
 
-def unpadding(RGBAfter): # 4
+def unpadding(RGBAfter):  # 4
     R, G, B = separateRGB(RGBAfter)
-    
+
     R = R[:h, :w]
     G = G[:h, :w]
     B = B[:h, :w]
@@ -256,10 +285,10 @@ def unpadding(RGBAfter): # 4
     return joinRGB(R, G, B)
 
 
-def YCbCrtoRGB(YCbCr): # 5
-    matrix = np.array([[0.299, 0.587, 0.114], 
-                    [-0.168736, -0.331264, 0.5], 
-                    [0.5, -0.418688, -0.081312]])
+def YCbCrtoRGB(YCbCr):  # 5
+    matrix = np.array([[0.299, 0.587, 0.114],
+                       [-0.168736, -0.331264, 0.5],
+                       [0.5, -0.418688, -0.081312]])
 
     inverted = np.linalg.inv(matrix)
     YCbCr[:, :, [1, 2]] -= 128
@@ -272,10 +301,9 @@ def YCbCrtoRGB(YCbCr): # 5
     return RGB
 
 
-def upSample(YD, CbD, CrD, dSample): # 6
+def upSample(YD, CbD, CrD, dSample):  # 6
     CbU = np.repeat(CbD, 2, axis=1)
     CrU = np.repeat(CrD, 2, axis=1)
-    
 
     if np.shape(YD)[0] % 2 != 0:
         CbU = np.delete(CbU, -1, 0)
@@ -294,7 +322,7 @@ def upSample(YD, CbD, CrD, dSample): # 6
     return YCbCrU
 
 
-def showYCbCr(YCbCr): # 5
+def showYCbCr(YCbCr):  # 5
     Y, Cb, Cr = separateYCbCr(YCbCr)
 
     fig = plt.figure(figsize=(10, 5))
@@ -311,58 +339,67 @@ def showYCbCr(YCbCr): # 5
     plt.imshow(Cr, cmGray)
 
 
-def calcIDCT(Y_dct, Cb_dct, Cr_dct):
-    Y = idct(idct(Y_dct, norm='ortho').T, norm='ortho').T
-    Cb = idct(idct(Cb_dct, norm='ortho').T, norm='ortho').T
-    Cr = idct(idct(Cr_dct, norm='ortho').T, norm='ortho').T
-    
-    return Y, Cb, Cr
-
-
-def calcIDCT_8x8_64x64(Y_dct, Cb_dct, Cr_dct, option): # 7.2 and 7.3
-    if option == 8 or option == 64:
-        Y_lines, Y_cols = np.shape(Y_dct) 
+def calcIDCT_8x8_64x64(Y_dct, Cb_dct, Cr_dct):  # 7
+    if opt == 8 or opt == 64:
+        Y_lines, Y_cols = np.shape(Y_dct)
         Cb_lines, Cb_cols = np.shape(Cb_dct)
 
-        Yx = Y_lines // option
-        Yy = Y_cols // option
-        Cx = Cb_lines // option
-        Cy = Cb_cols // option
+        Y = Y_dct
+        Cb = Cb_dct
+        Cr = Cr_dct
 
-        Y_sliced = Y_dct.reshape(Yx * Yy, option, option)
-        Cb_sliced = Cb_dct.reshape(Cx * Cy, option, option)
-        Cr_sliced = Cr_dct.reshape(Cx * Cy, option, option)
+        for i in range(0, Y_lines, opt):
+            for j in range(0, Y_cols, opt):
+                Y[i:i+opt, j:j+opt] = idct(idct(Y[i:i + opt,j:j + opt], norm="ortho").T, norm="ortho").T
 
-        i = 0
-        for i in range(len(Y_sliced)):
-            Y_sliced[i] = idct(idct(Y_sliced[i], norm="ortho").T, norm="ortho").T
-            i += 1
+        for i in range(0, Cb_lines, opt):
+            for j in range(0, Cb_cols, opt):
+                Cb[i:i + opt, j:j + opt] = idct(idct(Cb[i:i + opt, j:j + opt], norm="ortho").T, norm="ortho").T
+                Cr[i:i + opt, j:j + opt] = idct(idct(Cr[i:i + opt, j:j + opt], norm="ortho").T, norm="ortho").T
 
-        i = 0
-        for i in range(len(Cb_sliced)):
-            Cb_sliced[i] = idct(idct(Cb_sliced[i], norm="ortho").T, norm="ortho").T
-            Cr_sliced[i] = idct(idct(Cr_sliced[i], norm="ortho").T, norm="ortho").T
-            i += 1
-        
-        Y = Y_sliced.reshape(Y_lines, Y_cols)
-        Cb = Cb_sliced.reshape(Cb_lines, Cb_cols)
-        Cr = Cr_sliced.reshape(Cb_lines, Cb_cols)
-    
+    else:
+        Y = idct(idct(Y_dct, norm='ortho').T, norm='ortho').T
+        Cb = idct(idct(Cb_dct, norm='ortho').T, norm='ortho').T
+        Cr = idct(idct(Cr_dct, norm='ortho').T, norm='ortho').T
+
     return Y, Cb, Cr
+
+
+def deQuantization(Y_Q, Cb_Q, Cr_Q):    # 8
+    Q_Y, Q_CbCr = getQuantizationMatrix(requiredQuality=75)
+
+    Y_lines, Y_cols = np.shape(Y_Q)
+    Cb_lines, Cb_cols = np.shape(Cb_Q)
+
+    for i in range(0, Y_lines, opt):
+        for j in range(0, Y_cols, opt):
+            Y_Q[i:i + opt, j:j + opt] = np.round(Y_Q[i:i + opt, j:j + opt] * Q_Y)
+                
+    for i in range(0, Cb_lines, opt):
+        for j in range(0, Cb_cols, opt):
+            Cb_Q[i:i + opt, j:j + opt] = np.round(Cb_Q[i:i + opt, j:j + opt] * Q_CbCr)
+            Cr_Q[i:i + opt, j:j + opt] = np.round(Cr_Q[i:i + opt, j:j + opt] * Q_CbCr)
+    
+    return Y_Q, Cb_Q, Cr_Q
 
 
 def main():
     plt.close('all')
 
-    Y_dct, Cb_dct, Cr_dct, dSample = encoder()
-    decoder(Y_dct, Cb_dct, Cr_dct, dSample)
+    Y_Q, Cb_Q, Cr_Q = encoder()
+    decoder(Y_Q, Cb_Q, Cr_Q)
 
     plt.show()
 
 
 if __name__ == '__main__':
-    cmr = clr.LinearSegmentedColormap.from_list('cmr', [(0, 0, 0), (1, 0, 0)], N=256)
-    cmg = clr.LinearSegmentedColormap.from_list('cmg', [(0, 0, 0), (0, 1, 0)], N=256)
-    cmb = clr.LinearSegmentedColormap.from_list('cmb', [(0, 0, 0), (0, 0, 1)], N=256)
-    cmGray = clr.LinearSegmentedColormap.from_list('cmGray', [(0, 0, 0), (1, 1, 1)], N=256)
+    cmr = cb_linesr.LinearSegmentedColormap.from_list(
+        'cmr', [(0, 0, 0), (1, 0, 0)], N=256)
+    cmg = cb_linesr.LinearSegmentedColormap.from_list(
+        'cmg', [(0, 0, 0), (0, 1, 0)], N=256)
+    cmb = cb_linesr.LinearSegmentedColormap.from_list(
+        'cmb', [(0, 0, 0), (0, 0, 1)], N=256)
+    cmGray = cb_linesr.LinearSegmentedColormap.from_list(
+        'cmGray', [(0, 0, 0), (1, 1, 1)], N=256)
+
     main()
